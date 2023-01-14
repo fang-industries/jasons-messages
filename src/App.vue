904b35329cd869eba72724d4ca4de2156bbc69c9
jason <pnxl@pm.me>
2023-01-15 03:25:51 +0700

    darker light bg on main views

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="app"
-    class="min-h-screen min-w-full bg-neutral-100 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200"
+    class="min-h-screen min-w-full bg-neutral-200 text-neutral-800 dark:bg-neutral-900 dark:text-neutral-200"
   >
     <router-view v-slot="{ Component }">
       <transition name="fade" mode="out-in">
