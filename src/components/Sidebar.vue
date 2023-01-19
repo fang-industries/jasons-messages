a7964a30d846c0987431dc0350ce7cf6923cd9a4
jason <pnxl@pm.me>
2023-01-20 04:00:27 +0700

    add border to light mode

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,8 +1,14 @@
 <template>
   <div
-    class="h-screen w-1/4 overflow-y-auto bg-neutral-100 dark:bg-neutral-800"
+    class="h-screen w-1/4 overflow-y-auto border-r-2 border-neutral-200 bg-neutral-100 dark:border-neutral-800 dark:bg-neutral-800"
   >
-    <h1 class="p-6 text-lg">Messages</h1>
+    <h1 class="p-6 text-lg shadow-lg">Messages</h1>
+    <Conversation
+      img="https://fangdustry.me/assets/jason.929209c1.png"
+      name="Jason fang"
+      msg="aaaa"
+      time="24-24-24"
+    />
   </div>
 </template>
 
