051137453522dc45407593efccbdc04973370c01
jason <pnxl@pm.me>
2023-01-20 04:17:39 +0700

    add messages and container

---
+++ src/components/Container.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex w-3/4 justify-center bg-neutral-100 dark:bg-neutral-900">
+    <div class="h-screen w-full overflow-y-auto">
+      <h1
+        class="fixed top-0 w-full bg-neutral-100 bg-opacity-90 p-6 text-lg shadow-xl backdrop-blur-md dark:bg-neutral-900 dark:bg-opacity-90"
+      >
+        {{ contact }}
+      </h1>
+      <div class="overflow-y-auto">
+        <div class="w-full py-32 px-6">
+          <div class="flex flex-col gap-y-12">
+            <div class="flex flex-col gap-y-4">
+              <p
+                class="text-center text-sm text-neutral-600 dark:text-neutral-400"
+              >
+                {{ time }}
+              </p>
+              <div class="flex justify-center">
+                <p
+                  class="w-fit rounded-md bg-neutral-200 bg-opacity-50 py-2 px-4 text-center text-sm text-neutral-600 dark:bg-neutral-800 dark:text-neutral-400"
+                >
+                  Texting with {{ contact }} ({{ platform }})
+                </p>
+              </div>
+            </div>
+            <div class="flex w-full flex-col gap-y-4">
+              <slot />
+            </div>
+          </div>
+        </div>
+        <div
+          class="fixed bottom-0 flex w-3/4 justify-center bg-neutral-100 bg-opacity-90 p-6 text-neutral-600 backdrop-blur-md dark:bg-neutral-700 dark:bg-opacity-90 dark:text-neutral-400"
+        >
+          <p>Texting with {{ contact }} is not available right now.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  time: string;
+  platform: string;
+  contact: string;
+}>();
+</script>
---
+++ src/components/Message.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex min-w-full">
+    <div
+      v-if="props.alignment === 'left'"
+      class="flex min-w-full justify-start gap-x-6 overflow-x-hidden"
+    >
+      <img
+        v-if="img"
+        :src="img"
+        :class="
+          props.alignment === 'left'
+            ? 'mt-auto h-10 w-10 rounded-full'
+            : 'hidden'
+        "
+      />
+      <div
+        v-if="props.alignment === 'left'"
+        :class="
+          props.alignment === 'left' && !img
+            ? 'ml-16 max-w-[50%] rounded-2xl bg-neutral-200 p-3 dark:bg-neutral-800'
+            : 'max-w-[50%] rounded-2xl bg-neutral-200 p-3 dark:bg-neutral-800'
+        "
+      >
+        <p class="h-fit break-all">
+          <slot />
+        </p>
+      </div>
+    </div>
+    <div
+      v-if="props.alignment === 'right'"
+      class="flex min-w-full justify-end gap-x-6 overflow-x-hidden"
+    >
+      <div
+        v-if="props.alignment === 'right'"
+        class="max-w-[50%] rounded-2xl bg-blue-100 p-3 text-neutral-900 dark:bg-blue-300"
+      >
+        <p class="h-fit break-all">
+          <slot />
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  img?: string;
+  alignment: "right" | "left";
+}>();
+</script>
