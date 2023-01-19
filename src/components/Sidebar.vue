b7089afac811d39c55c9f2a8e59ebd7623425b25
jason <pnxl@pm.me>
2023-01-20 04:41:45 +0700

    first story woo

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,7 +3,11 @@
     class="h-screen w-1/4 overflow-y-auto border-r-2 border-neutral-200 bg-neutral-100 dark:border-neutral-800 dark:bg-neutral-800"
   >
     <h1 class="p-6 text-lg shadow-lg">Messages</h1>
-    <Conversation name="Jason fang" msg="aaaa" time="24-24-24" />
+    <Conversation
+      name="“I want to say thank you.”"
+      msg=" Truth is, I don’t think I deserve to be with you. I’ve lied to you, I’ve hurt you, left you, made promises and never kept them."
+      time="24-24-24"
+    />
   </div>
 </template>
 
---
+++ src/pages/i-want-to-say-thank-you.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="flex">
+    <Sidebar />
+    <Container name="“I want to say thank you.”" time="20-01-2023">
+      <Message alignment="right"> Are you busy? </Message>
+      <Message alignment="left"> Hey - why are you texting me? </Message>
+      <Message alignment="right">
+        I’m sorry, can you just hear me out; if that’s okay?
+      </Message>
+      <Message alignment="right">
+        If it sounds like I’m rambling, feel free to cut me off.
+      </Message>
+      <Message alignment="left">
+        You know I wouldn’t do that - even if you were; but I’ll listen
+      </Message>
+      <Message alignment="right"> I want to say thank you. </Message>
+      <Message alignment="right">
+        I know we broke up months ago, we’ve been through things..
+      </Message>
+      <Message alignment="right">
+        You’ve even stated that it’s best we aren’t friends either, but want to
+        be friends.
+      </Message>
+      <Message alignment="right">
+        I’ve tried and tried to forget you like you asked, sometimes I do, then,
+        sometimes you take over my mind for days, weeks.
+      </Message>
+      <Message alignment="right">
+        The one thing that’s taken up my mind the most is when you asked me
+        “What even is it about me?”. At the time, I didn’t truly didn’t know how
+        to answer that question. I didn’t know how to put my feelings into words
+        properly.
+      </Message>
+      <Message alignment="right">
+        But, now I do. You treated me like no one ever has, we had a connection
+        that seemed surreal, the way we understood each other was like we were
+        made for each other. The person you are, the person you were. You were
+        amazing. How can someone not love and cherish someone like you? You
+        never had the right mental state yet you always put others above
+        yourself.
+      </Message>
+      <Message alignment="left">
+        Thank you. Unfortunately, I think this conversation should end.
+      </Message>
+      <Message alignment="right">
+        Why? Why do you always fucking run away from your feelings? Please, for
+        once. Just face your fears and problems. Tell me how you feel about
+        this. I know I asked you to listen and know I said you can cut me off
+        whenever but please for once.
+      </Message>
+      <Message alignment="left"> Fine. </Message>
+      <Message alignment="left">
+        Truth is, I don’t think I deserve to be with you. I’ve lied to you, I’ve
+        hurt you, left you, made promises and never kept them. Why do you keep
+        coming back to me? I always leave but you always end up forgiving me. I
+        don’t want to seem like I’m taking advantage of you. I’m never in the
+        right mental state when we meet, yet you always know how to deal with
+        it. I’ve done so much to you, put so much on you and hurt you. I can
+        never forgive myself for that and I’ll never understand how you can
+        forgive me.
+      </Message>
+    </Container>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Sidebar from "@/components/Sidebar.vue";
+import Container from "@/components/Container.vue";
+import Message from "@/components/Message.vue";
+</script>
