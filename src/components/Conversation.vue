b321b56c518bce54ca0ff8e59d683eba51b27362
jason <pnxl@pm.me>
2023-01-20 04:41:36 +0700

    use same formula

--- src/components/Conversation.vue
+++ src/components/Conversation.vue
@@ -10,7 +10,14 @@
         .replaceAll(',', '')
     "
     :class="
-      $route.name === props.name.toLowerCase().replaceAll(' ', '-')
+      $route.name ===
+      props.name
+        .toLowerCase()
+        .replaceAll(' ', '-')
+        .replaceAll('”', '')
+        .replaceAll('“', '')
+        .replaceAll('.', '')
+        .replaceAll(',', '')
         ? 'flex justify-between bg-neutral-300 bg-opacity-40 px-6 py-4 dark:bg-neutral-700'
         : 'flex justify-between px-6 py-4 transition duration-300 hover:bg-neutral-300 hover:bg-opacity-40 dark:hover:bg-neutral-700'
     "
@@ -18,9 +25,10 @@
     <div class="flex gap-x-4">
       <div class="flex flex-col">
         <h2>{{ props.name }}</h2>
-        <span class="text-neutral-600 dark:text-neutral-400">{{
-          props.msg
-        }}</span>
+        <span
+          class="max-w-full overflow-x-hidden text-ellipsis whitespace-nowrap text-neutral-600 dark:text-neutral-400"
+          >{{ props.msg }}</span
+        >
       </div>
     </div>
     <span class="text-sm text-neutral-600 dark:text-neutral-400">{{
